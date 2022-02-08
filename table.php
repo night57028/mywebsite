c0bc5c8fa4c34354e4ead413a70b988dcb649a29
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-08 14:48:59 +0700

    Update table.php

--- table.php
+++ table.php
@@ -14,7 +14,7 @@
         <td>1</td>
         <td>kim jisoo</td>
         <td>jisoo</td>
-        <td><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brighttv.co.th%2Fentertain%2F%25E0%25B9%2588jisoo-blackpink-yg&psig=AOvVaw2PtZG3hchVMlbV0oKKknEw&ust=1644392330529000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMisj_jM7_UCFQAAAAAdAAAAABAD.jpeg" width="130px"></td>
+        <td><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brighttv.co.th%2Fentertain%2F%25E0%25B9%2588jisoo-blackpink-yg&psig=AOvVaw2PtZG3hchVMlbV0oKKknEw&ust=1644392330529000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMisj_jM7_UCFQAAAAAdAAAAABAD" width="130px"></td>
       </tr>  
     </table>
   </body> 
