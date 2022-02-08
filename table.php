6fa2ce7caf51af267ed919d5c9dae63f8894f9d6
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-08 14:29:27 +0700

    Update table.php

--- table.php
+++ table.php
@@ -5,9 +5,17 @@
   <body>
     <table border="1">
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
       </tr>
+      <tr>
+        <td>1</td>
+        <td>จิรภิญญา อินทร์แก้ว</td>
+        <td>ไนท์</td>
+        <td><ing ></hd>
+      </tr>  
     </table>
   </body> 
 </html>
