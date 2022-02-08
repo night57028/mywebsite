a3332a0013295e76fee50ec29d760fcf126db782
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-08 15:17:25 +0700

    Update table.php

--- table.php
+++ table.php
@@ -9,19 +9,33 @@
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
         <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
       <tr>
         <td>1</td>
         <td>kim jisoo</td>
         <td>jisoo</td>
         <td><img src="https://i.pinimg.com/originals/c5/0a/46/c50a46f12c99102d113ed344fa6390d0.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
         <tr>
         <td>1</td>
         <td>kim jennie</td>
         <td>jennie</td>
         <td><img src="https://i.pinimg.com/736x/7d/e1/0b/7de10be8df846f5531abcceeb448ef8c.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>1</td>
+        <td>park chaeyoung</td>
+        <td>rosé</td>
+        <td><img src="https://www.brighttv.co.th/wp-content/uploads/2021/03/rose-blackpink-thanks-fans%E0%B8%9B%E0%B8%81.png" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>1</td>
+        <td>lalisa manobal</td>
+        <td>lisa</td>
+        <td><img src="https://s359.kapook.com/pagebuilder/a842997f-38e7-4be4-aa8c-eedfc2efd45c.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
       </tr>
-      </tr>  
     </table>
   </body> 
 </html>
