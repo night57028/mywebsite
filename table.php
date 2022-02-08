aca5b46a9068697fbac7b72f890ef4e5bf8377bd
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-08 14:59:39 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,6 +15,12 @@
         <td>kim jisoo</td>
         <td>jisoo</td>
         <td><img src="https://i.pinimg.com/originals/c5/0a/46/c50a46f12c99102d113ed344fa6390d0.jpg" width="100px"></td>
+        <tr>
+        <td>1</td>
+        <td>kim jennie</td>
+        <td>jennie</td>
+        <td><img src="https://i.pinimg.com/736x/7d/e1/0b/7de10be8df846f5531abcceeb448ef8c.jpg" width="100px"></td>
+      </tr>
       </tr>  
     </table>
   </body> 
