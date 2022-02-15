84be49d01aa1bc2d93e45292d22fb5e629a7ae6f
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-15 13:11:27 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+	</head>
+	<body>
+		<p style="color:green;">ข้อความนี้สีเขียวปกติ</p>
+		<p style="color:#82933f;">ข้อความนี้สีเขียวรหัสสี</p>
+	</body>
+</html>
