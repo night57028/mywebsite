9a2fd9ff7753cc3c5a84e3be32fef786bbfa28ff
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-22 13:59:19 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url()
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url()
+    }
+    #font1{
+	font-family: myFont1;    
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">น.ส.จิรภิญญา อินทร์แก้ว เลขที่ 17 เลขประจำตัว 57028</h1>
+  <h1 id="font2">น.ส.จิรภิญญา อินทร์แก้ว เลขที่ 17 เลขประจำตัว 57928</h1>
+</body>
+</html>
