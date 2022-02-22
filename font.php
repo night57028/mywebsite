118c4280f712cc3e2cd5185e761af7e152d80336
night57028 <97937646+night57028@users.noreply.github.com>
2022-02-22 14:14:59 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,7 +6,7 @@
   <style>
     @font-face{
       font-family: myFont1;
-      src: url()
+      src: url(https://github.com/night57028/mywebsite/blob/main/DM%20Shining%20Star%20Regular.woff)
     }
     @font-face{
       font-family: myFont2;
